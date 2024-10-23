46e79bf283d6dc569b43eebdc49bad453a2c11c3
Kenechukwu Omalu <kenechukwu.omalu@pau.edu.ng>
2024-10-23 14:16:41 -0700

    (Class project for week 2)

---
+++ week-2/project-1.rs
@@ -0,0 +1,11 @@
+fn main() {
+    let  p = 520000;
+    let  r = 10;
+    let  t = 5;
+    // simple intrest
+    let  a = p * (1 + (r / 100)) * t;
+    println!("a is : {}" , a);
+    let si = a - p;
+    println!(" simple intrest is {}", si );
+
+}
